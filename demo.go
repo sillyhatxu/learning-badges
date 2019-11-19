abf94c852a5dffddee86f361edb41534aa0d7719
shikuan.xu <shikuan.xu@advance.ai>
2019-11-19 16:22:32 +0800

    initial product

--- demo.go
+++ demo.go
@@ -1,5 +1,41 @@
 package learning_badges
 
-func Print() string {
+func Print1() string {
+	return "hello world"
+}
+
+func Print2() string {
+	return "hello world"
+}
+
+func Print3() string {
+	return "hello world"
+}
+
+func Print4() string {
+	return "hello world"
+}
+
+func Print5() string {
+	return "hello world"
+}
+
+func Print6() string {
+	return "hello world"
+}
+
+func Print7() string {
+	return "hello world"
+}
+
+func Print8() string {
+	return "hello world"
+}
+
+func Print9() string {
+	return "hello world"
+}
+
+func Print10() string {
 	return "hello world"
 }
--- demo_test.go
+++ demo_test.go
@@ -5,8 +5,49 @@ import (
 	"testing"
 )
 
-func TestPrint(t *testing.T) {
-	expected := "hello world"
-	result := Print()
+const expected = "hello world"
+
+func TestPrint1(t *testing.T) {
+	result := Print1()
+	assert.EqualValues(t, expected, result)
+}
+
+func TestPrint2(t *testing.T) {
+	result := Print2()
+	assert.EqualValues(t, expected, result)
+}
+
+func TestPrint3(t *testing.T) {
+	result := Print3()
+	assert.EqualValues(t, expected, result)
+}
+
+func TestPrint4(t *testing.T) {
+	result := Print4()
+	assert.EqualValues(t, expected, result)
+}
+
+func TestPrint5(t *testing.T) {
+	result := Print5()
+	assert.EqualValues(t, expected, result)
+}
+
+func TestPrint6(t *testing.T) {
+	result := Print6()
+	assert.EqualValues(t, expected, result)
+}
+
+func TestPrint7(t *testing.T) {
+	result := Print7()
+	assert.EqualValues(t, expected, result)
+}
+
+func TestPrint8(t *testing.T) {
+	result := Print8()
+	assert.EqualValues(t, expected, result)
+}
+
+func TestPrint9(t *testing.T) {
+	result := Print9()
 	assert.EqualValues(t, expected, result)
 }
